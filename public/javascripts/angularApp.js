14ec7dd5e246bade0a91992c30bd3e35253ff29b
inesfranch <ifranch@princeton.edu>
2016-05-02 23:09:07 -0400

    Fixed product display

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -13,9 +13,6 @@ app.config([
       controller: 'MainCtrl',
       resolve: {
         postPromise: ['$stateParams', 'products', function($stateParams, products){
-          console.log("Opening Home Page...");
-          console.log($stateParams.category);
-          console.log($stateParams.query);
           return products.search($stateParams.category, $stateParams.query);
         }]
       }
@@ -163,13 +160,10 @@ app.factory('products', ['$http', 'auth', '$window', function($http, auth, $wind
     });
   };
   o.search = function(cat, q) {
-    console.log("Search Function entered...");
     if (q == null) q = "";
     else q = q.toString();
-    console.log("Calling HTTP Search req...");
     return $http.get("/search?cat="+cat+"&q="+q).success(function(data) {
       angular.copy(data, o.products);
-      console.log("HTTP Search req Returns");
     });
   };
   o.matchNotifications = function(product) {
@@ -196,6 +190,8 @@ app.factory('products', ['$http', 'auth', '$window', function($http, auth, $wind
       var token = res.data.token;
       window.localStorage['tigermart-token'] = token;
       var user = JSON.parse($window.atob(token.split('.')[1]));
+      console.log(user.posted);
+      console.log(res.data);
       sessionStorage.setItem('user', JSON.stringify(res.data));
       return res.data;
     });
@@ -213,7 +209,6 @@ app.factory('products', ['$http', 'auth', '$window', function($http, auth, $wind
   o.editProduct = function(product, id, user) {
     return $http.put('/products/' + id, product, user).success(function(data){
       console.log(data);
-      console.log("Product Edited...");
       $http.get('/products?cat=All').success(function(data){
         angular.copy(data, o.products);
       });
@@ -223,13 +218,9 @@ app.factory('products', ['$http', 'auth', '$window', function($http, auth, $wind
   o.setNotifications= function(notification, user) {
     id = user._id.toString();
     notification = notification.toString();
-    console.log(id);
-    console.log(notification);
     return $http.put("/setNotifications/"+id+"?notification="+notification).success(function(data){
-      console.log(user.notifications);
       user.notifications.push(notification);
       auth.saveToken(data.token);
-      console.log(user.notifications);
       //sessionStorage.setItem('user', JSON.stringify(user));
       
       });
@@ -249,7 +240,6 @@ app.factory('products', ['$http', 'auth', '$window', function($http, auth, $wind
     var description = product.description;
     var price = product.price;
     var id = product._id;
-    console.log("Send function called");
     var to = user.email;
     to = to.toString();
     subject = "A product you were looking for is now available!";
@@ -260,7 +250,6 @@ app.factory('products', ['$http', 'auth', '$window', function($http, auth, $wind
   o.changeProductAvail = function(id) {
     return $http.put('/products/changeAvail/' + id).success(function(data){
       console.log(data); 
-      console.log("Product Availability changed...");
       $http.get('/products?cat=All').success(function(data){
         angular.copy(data, o.products);
       });
@@ -269,7 +258,6 @@ app.factory('products', ['$http', 'auth', '$window', function($http, auth, $wind
   o.delProduct = function(productID, userID) {
     return $http.delete('/products/' + productID + '/' + userID).success(function(data){
       console.log(data);
-      console.log("Product Deleted...");
       $http.get('/products?cat=All').success(function(data){
         angular.copy(data, o.products);
       });
@@ -436,7 +424,6 @@ function($scope, products, product, $state, auth){
     }
   };
   $scope.sameUser = function() {
-    console.log("hello friend");
     var b = ($scope.user.net_id == product.userid.net_id);
     console.log(b);
     return b;
@@ -451,7 +438,6 @@ function($scope, products, product, $state, auth){
     console.log(auth.currentUser());
     if(!$scope.cat || $scope.cat === '') {$scope.cat = "All";}
     $state.go('home', {category: $scope.cat, query: $scope.q});
-    console.log("x");
   };
   /*$scope.linkToCat = function(cat){
     products.getAll(cat).error(function(error){
@@ -502,7 +488,6 @@ $scope.search = function(){
   console.log(auth.currentUser());
   if(!$scope.cat || $scope.cat === '') {$scope.cat = "All";}
   $state.go('home', {category: $scope.cat, query: $scope.q});
-  console.log("x");
 };
 
 $scope.editProduct = function(dataUrl1){
@@ -569,8 +554,9 @@ function($scope, products, $state, auth){
   $scope.user = auth.currentUser();
   user = $scope.user;
   
+  console.log(user);
+
   if($state.params.id != $scope.user._id) {
-    console.log("hello2");
     $state.go('home', {category: "All", query: ""});
   } 
 
@@ -578,7 +564,6 @@ function($scope, products, $state, auth){
   console.log(auth.currentUser());
   if(!$scope.cat || $scope.cat === '') {$scope.cat = "All";}
   $state.go('home', {category: $scope.cat, query: $scope.q});
-  console.log("x");
   //products.search($scope.q);
   };
 
@@ -745,7 +730,6 @@ function($scope, products, $state, auth, user2){
     console.log(auth.currentUser());
     if(!$scope.cat || $scope.cat === '') {$scope.cat = "All";}
     $state.go('home', {category: $scope.cat, query: $scope.q});
-    console.log("x");
   };
 
   $scope.logOut = function(){
@@ -770,11 +754,9 @@ function($scope, products, $state, auth){
   $scope.cat = "All";
 
   $scope.editUser = function() {
-    console.log("hello");
     auth.editUser(user, user._id).error(function(error){
       $scope.error = error;
     }).success(function() { 
-      console.log("hello2");
       $state.go('users', {id: $scope.user._id}); 
     });
   };
@@ -787,7 +769,6 @@ function($scope, products, $state, auth){
     console.log(auth.currentUser());
     if(!$scope.cat || $scope.cat === '') {$scope.cat = "All";}
     $state.go('home', {category: $scope.cat, query: $scope.q});
-    console.log("x");
   };
   $scope.logOut = function(){
     auth.logOut();
@@ -820,11 +801,9 @@ app.controller('SetNotificationsCtrl', [
     };
 
     $scope.setNotifications = function(){
-      console.log($scope.notification);
       products.setNotifications($scope.notification, $scope.user).error(function(error) {
         $scope.error = error;
       }).then(function() {
-        console.log($scope.user);
         $state.go($state.current, {}, {reload:true});
       });
       $scope.notification = '';
@@ -838,7 +817,6 @@ app.controller('SetNotificationsCtrl', [
       console.log(auth.currentUser());
       if(!$scope.cat || $scope.cat === '') {$scope.cat = "All";}
       $state.go('home', {category: $scope.cat, query: $scope.q});
-      console.log("x");
     };
 
     $scope.logOut = function(){
@@ -888,7 +866,6 @@ function($scope, products, $state, auth){
     console.log(auth.currentUser());
     if(!$scope.cat || $scope.cat === '') {$scope.cat = "All";}
     $state.go('home', {category: $scope.cat, query: $scope.q});
-    console.log("x");
   };
 
   $scope.addProduct = function(dataUrl1){
@@ -896,9 +873,6 @@ function($scope, products, $state, auth){
     var user = $scope.user;
 
     console.log($scope.title);
-
-    //console.log(document.getElementById("mySingleField").value);
-    //console.log($scope.tags);
 
     var picURL = dataUrl1.split("base64,")[1];
     if (!$scope.picFile1)
@@ -912,7 +886,6 @@ function($scope, products, $state, auth){
       description: $scope.description,
       price: $scope.price,
       pictures: picURL,
-      //tags: "tags",
       date: new Date(),
       month: ((new Date()).getMonth() + 1),
       day: (new Date()).getDate(),
@@ -927,21 +900,6 @@ function($scope, products, $state, auth){
       $state.go('home', {category: "All", query: ""});
 
     });
-    // ADD VALIDATIONS LATER!
-    /*products.create({
-      
-    }, user).error(function(error) {
-      $scope.error = error;
-    }).then(function() {
-      $state.go('home');
-    });
-    $scope.title = '';
-    $scope.category = '';
-    $scope.description = '';
-    $scope.price = '';
-    $scope.picFile1 = '';
-    //$scope.tags = '';
-    */
   };
 
   $scope.logOut = function(){
@@ -950,29 +908,7 @@ function($scope, products, $state, auth){
   };
 }]);
 
-/*app.controller('WelcomeCtrl', [
-'$scope',
-'$state',
-'products',
-function($scope, $state, products){
 
-  $scope.getUser = function() {
-    products.getUser($scope.user).error(function(error){
-      $scope.error = error;
-    }).then(function() {
-      $state.go('home');
-    });
-  };
-
-  $scope.register = function() {
-    products.register($scope.user).error(function(error) {
-      $scope.error = error;
-    }).then(function() {
-      $state.go('home');
-    });
-
-  };
-}]);*/
 
 app.controller('AuthCtrl', [
 '$scope',
@@ -999,7 +935,6 @@ function($scope, $state, auth){
       $scope.error = error;
       $scope.user.password = "";
     }).then(function(){
-      console.log("Logging in...");
       $state.go('home', {category: "All", query: ""});
     });
     
